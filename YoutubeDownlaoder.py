2883dfd80ec7a17f7aa84a57942169033403d2b1
Vishwet Nadimetla <103368238+mr-vishwet@users.noreply.github.com>
2023-03-16 13:38:44 +0530

    Add files via upload

---
+++ YoutubeDownlaoder.py
@@ -0,0 +1,77 @@
+# YoutubeDownlaoder.py
+import streamlit as st
+from pytube import YouTube
+
+
+def download_video(video_url, resolution, file_path):
+    yt = YouTube(video_url)
+    stream = yt.streams.filter(res=resolution).first()
+    stream.download(output_path=".", filename=file_path)
+
+
+def download_audio(video_url, quality, file_name):
+    yt = YouTube(video_url)
+    stream = yt.streams.filter(only_audio=True, abr=quality).first()
+    stream.download(output_path=".", filename=file_name)
+
+
+# Set page title
+st.set_page_config(page_title='YouTube Downloader')
+
+# Set page header
+st.header('YouTube Downloader')
+
+
+# Create form for video download
+with st.form('Video Download'):
+    # Ask user for YouTube video URL
+    video_url = st.text_input('Enter YouTube video URL:', '')
+    resolution = st.selectbox('Select video quality:', [
+                              '1080p', '720p', '480p', '360p', '240', '144p'])
+
+    # Define download button
+    if st.form_submit_button('Download Video'):
+        try:
+            file_name = "video.mp4"
+            download_video(video_url, resolution, file_name)
+
+            # Read the downloaded file as bytes
+            with open(file_name, 'rb') as f:
+                video_data = f.read()
+            st.download_button(
+                label="Click Here to Download",
+                data=video_data,
+                file_name=file_name,
+                mime='video/mp4',
+                file_name=file_name
+            )
+        except Exception as e:
+            st.write("Error:", e)
+
+# Create form for audio download
+with st.form('Audio Download'):
+    # Ask user for YouTube video URL
+    url = st.text_input('Enter YouTube video URL:', '')
+
+    # Ask user for audio quality
+    quality_options = ['128kbps', '192kbps', '256kbps']
+    quality = st.selectbox('Select audio quality:', quality_options)
+
+    # Define download button
+    if st.form_submit_button('Download Audio'):
+        # Set download path to Downloads folder by default
+        try:
+            # download the audio to the default download directory on the remote server
+            download_audio(video_url, quality, file_name)
+
+        # offer the downloaded audio as a download button
+            with open(file_name, "rb") as f:
+                audio_data = f.read()
+            st.download_button(
+                label="Click Here to Download",
+                data=audio_data,
+                mime="audio/mp3",
+                file_name=file_name
+            )
+        except Exception as e:
+            st.write("Error:", e)
