83e1fd1349022578710af0531980e4c276ec3ce3
Vishwet Nadimetla <103368238+mr-vishwet@users.noreply.github.com>
2023-03-16 20:00:23 +0530

    Update YoutubeDownloader.py

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -34,6 +34,7 @@ def download_audio(audio_url, quality, file_path):
         st.write(f"Audio size: {audio_size:.2f} MB")
         stream.download(output_path=".", filename=file_path)
     else:
+        st.write("Audio quality not found")
         diffs = [abs(int(s.abr[:-4]) - int(quality[:-4])) for s in audio_streams]
         closest_stream = audio_streams[diffs.index(min(diffs))]
         audio_size = audio_stream.filesize / (1024 * 1024)
